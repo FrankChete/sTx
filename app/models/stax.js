5a60cf801c28c6941f4d9c317be23ec68ec24ec8
FrankChete <fchete@xgcws1448.corp.xoom.com>
2015-03-09 19:33:43 -0600

    display slug logos on accounts items

--- app/models/stax.js
+++ app/models/stax.js
@@ -6,7 +6,10 @@ var Stax = DS.Model.extend({
   service: DS.attr('string'),
   number: DS.attr('number'),
   state: DS.attr('string'),
-  foto: DS.attr('string')
+  barcode: DS.attr('string'),
+  XoomTN: DS.attr('number'),
+  total: DS.attr('string'),
+  logoUrl: DS.attr('string')
 });
 
 Stax.reopenClass({
@@ -17,23 +20,28 @@ Stax.reopenClass({
            service: 'claro-phone-postpay',
            number: 40516589,
            state: 'ready',
-           foto: 'https://s3.amazonaws.com/assets.bluekite.com/services/gt/claro-phone-postpay/logos/tall.png'
+           total: "Q241.50",
+           logoUrl: 'https://s3.amazonaws.com/assets.bluekite.com/services/gt/claro-phone-postpay/logos/tall.png'
          },
          {
            id: 2,
            country: 'MX',
            service: 'telmex-phone-postpay',
            number: 2911333863,
-           state: 'invoice',
-           foto: 'X'
+           state: 'processed',
+           barcode: "29113338630000008333",
+           XoomTN: '842278423719',
+           total: "$383.00",
+           logoUrl: 'https://s3.amazonaws.com/assets.bluekite.com/services/mx/telmex-phone-postpay/logos/tall.png'
          },
          {
            id: 3,
-           country: 'GT',
-           service: 'claro-phone-postpay',
-           number: 40516589,
-           state: 'ready',
-           foto: ''
+           country: 'SV',
+           service: 'delsur-electricity-postpay',
+           number: 2343953,
+           state: 'processed',
+           total: "$3.82",
+           logoUrl: 'https://s3.amazonaws.com/assets.bluekite.com/services/sv/delsur-electricity-postpay/logos/tall.png'
          }
     ]
 });
